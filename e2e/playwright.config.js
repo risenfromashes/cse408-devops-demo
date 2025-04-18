3db85819df3855610e2cea3900e2bd5636bfa075
Ashrafur Rahman <risenfromashes@arkdebian.dino.sighs>
2025-04-18 21:19:16 +0600

    fix unit tests

---
+++ e2e/playwright.config.js
@@ -0,0 +1,32 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
+  use: {
+    baseURL: process.env.BASE_URL || 'http://client:3000',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'on-first-retry',
+  },
+  
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+  ],
+  
+  // The server is expected to be already running in our case
+  webServer: {
+    reuseExistingServer: true,
+  },
+});
